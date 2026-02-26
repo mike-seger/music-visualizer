fb347ec697da94a2ca76406677c01366dd0248e6
Michael Seger <michael@net128.com>
2026-02-26 22:51:58 +0100

    ...

--- src/js/Version.js
+++ src/js/Version.js
@@ -2,4 +2,4 @@
  *  Used as a `?version=` cache-buster on all hash-based resource URLs
  *  (meta.json, presets/<hash>.ext, previews/<hash>.<ext>).
  */
-export const visualizerVersion = '1.0.0'
+export const visualizerVersion = '1.0.1'
